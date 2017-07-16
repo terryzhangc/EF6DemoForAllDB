ce7c32ddc4a96a602a438ad7f41b27db899651ca
terry <zhangc@shinetechchina.com>
2017-07-16 11:33:59 +0800

    12

---
+++ TestMySql/Migrations/201707160331582_AddUserTable.cs
@@ -0,0 +1,29 @@
+namespace TestMySql.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUserTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(unicode: false),
+                        Description = c.String(unicode: false),
+                        CreateOn = c.DateTime(nullable: false, precision: 0),
+                        UpdateOn = c.DateTime(nullable: false, precision: 0),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
